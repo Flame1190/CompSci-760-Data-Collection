- Fix ssim loss - Fix memory overflow - Add batching - Misc changes

SuperSampling/model/loss.py
@@ -1,5 +1,5 @@
 import torch
-from pytorch_msssim import ssim
+from kornia.metrics import ssim as compute_ssim
 
 def nsrr_loss(output: torch.Tensor, target: torch.Tensor, w: float=1) -> torch.Tensor:
     """
@@ -8,9 +8,5 @@ def nsrr_loss(output: torch.Tensor, target: torch.Tensor, w: float=1) -> torch.T
     
     """
     # # SSIM currently seems broken?
-    # loss_ssim = 1 - ssim(output, target, data_range=255, size_average=True)
-    # return loss_ssim
-    l1_loss = torch.nn.L1Loss()
-    loss_l1 = l1_loss(output, target)
-    
-    return loss_l1 
+    loss_ssim = 1 - torch.mean(compute_ssim(output, target, 9))
+    return loss_ssim

SuperSampling/model/metric.py
@@ -1,7 +1,6 @@
 import torch
-import numpy as np
 import math
-from pytorch_msssim import ssim as _ssim
+from kornia.metrics import ssim as compute_ssim
 
 def psnr(img1: torch.Tensor, img2: torch.Tensor) -> float:
     assert img1.shape == img2.shape
@@ -14,5 +13,5 @@ def psnr(img1: torch.Tensor, img2: torch.Tensor) -> float:
 
 def ssim(img1: torch.Tensor, img2: torch.Tensor) -> float:
     assert img1.shape == img2.shape
-    
-    return _ssim(img1, img2, data_range=255, size_average=True)
+
+    return torch.mean(compute_ssim(img1, img2, 9))

SuperSampling/model/model.py
@@ -7,14 +7,26 @@ import torch
 from typing import List
 
 class NSRR(BaseModel):
-    def __init__(self, scale_factor: int = 2, num_frames: int = 5):
+    def __init__(self, 
+                 scale_factor: int = 2, 
+                 num_frames: int = 5, 
+                 enable_warping: bool = True, 
+                 upsample_mode: str | None = None
+                 ):
         super().__init__()
         # may not need given not converting to YCbCr
         self.feature_extraction_current = FeatureExtraction() 
         self.feature_extraction_previous = FeatureExtraction()
         
-        self.upsampler = ZeroUpsampling(scale_factor=scale_factor)
-        self.warper = BackwardsWarping(scale_factor=scale_factor)
+        if upsample_mode is not None:
+            self.upsampler = nn.Upsample(scale_factor=scale_factor, mode=upsample_mode)
+        else:
+            self.upsampler = ZeroUpsampling(scale_factor=scale_factor)
+
+        if enable_warping:
+            self.warper = BackwardsWarping(scale_factor=scale_factor)
+        else:
+            self.warper = lambda a, *_: a
 
         self.feature_reweighting = FeatureReweighting(num_frames=num_frames)
         self.reconstruction = Reconstruction(num_frames=num_frames)
@@ -188,7 +200,11 @@ class FeatureReweighting(BaseModel):
 
         scaled_weights = self.max_amplification * ((x + 1) / 2)
 
-        weighted_prev_frames = [scaled_weights[:, i, : :] * previous_frames[i] for i in range(len(previous_frames))]
+        weighted_prev_frames = [None] * len(previous_frames)
+
+        for i in range(len(previous_frames)):
+            weighted_prev_frames[i] = scaled_weights[:, i, :].unsqueeze(1) * previous_frames[i]
+
         return weighted_prev_frames
 
 class Reconstruction(BaseModel):