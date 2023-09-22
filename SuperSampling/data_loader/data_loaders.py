a6ca8f86137f556a20c137bbace6fa86f077edef
Hugh Parsons <hughparsonsnz@gmail.com>
2023-09-22 13:45:09 +1200

    cleanup data_loader

--- SuperSampling/data_loader/data_loaders.py
+++ SuperSampling/data_loader/data_loaders.py
@@ -14,104 +14,98 @@ from collections import deque
 from typing import Union, Tuple, List
 
 
-class NSRRDataLoader(BaseDataLoader):
+class SupersamplingDataLoader(BaseDataLoader):
     """
     Generate batch of data
     `for x_batch in data_loader:`
     `x_batch` is a list of 4 tensors, meaning `view, depth, motion, view_truth`
     each size is (batch x channel x height x width)
+
+    NOTE: validation should always be done by an independent data loader
+    
     """
     def __init__(self,
-                 data_dir: str,
-                 img_dirname: str,
+                 data_dirs: str,
+                 color_dirname: str,
                  depth_dirname: str,
                  motion_dirname: str,
                  batch_size: int,
+                 scale_factor: int,
+                 num_frames: int,
                  shuffle: bool = True,
-                 validation_split: float = 0.0,
                  num_workers: int = 1,
-                 downsample: Union[Tuple[int, int], List[int], int] = (2, 2),
-                 num_data: Union[int,None] = None,
-                 resize_factor : Union[int, None] = None,
-                 resize_dimensions : None | Tuple[int, int] = None,
-                 num_frames: int = 5,
+                 num_data: Union[int, None] = None,
+                 output_dimensions: Union[int, None] = None,
                  ):
-        assert (resize_factor is not None) != (resize_dimensions is not None)
-        dataset = NSRRDataset(data_dir,
-                              img_dirname=img_dirname,
+        self.dataset = SupersamplingDataset(data_dirs=data_dirs,
+                              color_dirname=color_dirname,
                               depth_dirname=depth_dirname,
                               motion_dirname=motion_dirname,
-                              downsample=downsample,
+                              scale_factor=scale_factor,
                               num_data=num_data,
-                              resize_factor = resize_factor,
-                              resize_dimensions = resize_dimensions,
-                              num_frames = num_frames,
+                              output_dimensions=output_dimensions,
+                              num_frames=num_frames,
                               )
-        super().__init__(dataset=dataset,
+        super().__init__(dataset=self.dataset,
                          batch_size=batch_size,
                          shuffle=shuffle,
-                         validation_split=validation_split,
+                         validation_split=0, # read doc string note
                          num_workers=num_workers,
                          )
 
-
-class NSRRDataset(Dataset):
+class SupersamplingDataset(Dataset):
     """
     Requires that corresponding view, depth and motion frames share the same name.
     """
     def __init__(self,
-                 data_dir: str,
-                 img_dirname: str,
+                 data_dirs: str | List[str],
+                 color_dirname: str,
                  depth_dirname: str,
                  motion_dirname: str,
-                 downsample: int = 2,
-                 num_data:Union[int, None] = 5,
-                 resize_factor:Union[int, None] = None,
-                 resize_dimensions: None | Tuple[int, int] = None,
-                 num_frames: int = 5,
-                 transform: nn.Module = None,
+                 scale_factor: int,
+                 num_frames: int,
+                 output_dimensions: None | Tuple[int, int] = None,
+                 num_data: Union[int, None] = None,
                  ):
-        assert (resize_factor is not None) != (resize_dimensions is not None)
         super().__init__()
 
-        self.data_dir = data_dir
-        self.img_dirname = img_dirname
+        self.data_dirs = data_dirs if isinstance(data_dirs, list) else [data_dirs]
+        self.color_dirname = color_dirname
         self.depth_dirname = depth_dirname
         self.motion_dirname = motion_dirname
 
-        self.resize_factor = resize_factor
-        self.resize_dimensions = resize_dimensions
+        self.scale_factor = scale_factor
+        self.output_dimensions = output_dimensions
 
-        self.downsample = downsample
+        self.transform = tf.transforms.Compose([
+            tf.Lambda(lambda x: x[[2,1,0],:,:]), # BGR to RGB (for cv2)
+            tf.ToTensor(),
+        ])
 
-        if transform is None:
-            self.transform = tf.ToTensor()
+        self.clips = {
+            data_dir: sorted(os.list(os.path.join(data_dir, self.color_dirname)), reverse=False) for data_dir in self.data_dirs
+        }
 
-        self.img_list = os.listdir(os.path.join(self.data_dir, self.img_dirname))
-        self.img_list = sorted(self.img_list, key=lambda keys:[ord(i) for i in keys], reverse=False)
-        
         if num_data is None:
-            num_data = len(self.img_list)
+            num_data = sum([len(clip) for clip in self.clips.values()])
 
         self.data_list = []
         # maintain a buffer for the last num_frames frames
         img_name_buffer = deque(maxlen=num_frames) 
 
-        for i, img_name in enumerate(self.img_list):
+        for i, clip in enumerate(self.clips.values()):
+            for img_name in clip:
+                if(i>=num_data + num_frames - 1):
+                    break
+                    
+                img_name_buffer.appendleft(img_name)
+                if len(img_name_buffer) == num_frames:
+                    self.data_list.append(list(img_name_buffer))
 
-            if(i>=num_data + num_frames - 1):
-                break
-                
             # handle scene change
-            # TODO: more complex scene names — currently just [a-zA-Z]\d+
-            if len(img_name_buffer) and img_name[0] != img_name_buffer[0][0]:
-                img_name_buffer.clear()
+            img_name_buffer.clear()
 
-            img_name_buffer.appendleft(img_name)
-
-            if len(img_name_buffer) == num_frames:
-                self.data_list.append(list(img_name_buffer))
-                
+    
     def __getitem__(self, index):
         data = self.data_list[index]
 
@@ -119,7 +113,7 @@ class NSRRDataset(Dataset):
         # elements in the lists following the order: current frame i, pre i-1, pre i-2, pre i-3, pre i-4
         for frame in data:
             frame, _ = frame.rsplit('.', 1)
-            img_path = os.path.join(self.data_dir, self.img_dirname, f"{frame}.png")
+            img_path = os.path.join(self.data_dir, self.color_dirname, f"{frame}.png")
             depth_path = os.path.join(self.data_dir, self.depth_dirname, f"{frame}.exr")
             motion_path = os.path.join(self.data_dir, self.motion_dirname, f"{frame}.exr")
             
@@ -129,29 +123,22 @@ class NSRRDataset(Dataset):
             
             
             width, height, _ = img_view_truth.shape
-            if self.resize_factor:
-                height, width = height//self.resize_factor, width//self.resize_factor
-            else:
+            if self.resize_dimensions is not None:
                 height, width = self.resize_dimensions # TODO: reorder, this should be width, height
                 
-
+            # TODO: check interpolation method
             img_view_truth = cv2.resize(img_view_truth, (height, width), cv2.INTER_NEAREST)
 
-            img_view_input = cv2.resize(img_view_truth, (height//self.downsample, width//self.downsample), cv2.INTER_NEAREST)
-            img_depth = cv2.resize(img_depth,  (height//self.downsample, width//self.downsample), cv2.INTER_NEAREST)
-            img_motion = cv2.resize(img_motion, (height//self.downsample, width//self.downsample), cv2.INTER_NEAREST)
+            low_res_height, low_res_width = height // self.scale_factor, width // self.scale_factor
+            img_view_input = cv2.resize(img_view_truth, (low_res_height, low_res_width), cv2.INTER_NEAREST)
+            img_depth = cv2.resize(img_depth,  (low_res_height, low_res_width), cv2.INTER_NEAREST)
+            img_motion = cv2.resize(img_motion, (low_res_height, low_res_width), cv2.INTER_NEAREST)
 
-            target_image = self.transform(img_view_truth)[[2,1,0],:,:]
-            img_view = self.transform(img_view_input)[[2,1,0],:,:]
-
-            # depth data is in the 3rd channel (channels are BGR)
-            img_depth = self.transform(img_depth)[2:3, :, :]
-            
-            # motion data is in the 2nd and 3rd channels (channels are BGR)
-            img_motion = self.transform(img_motion)[1:3,:,:] 
-            # swap channels to match the order of the motion vectors
-            img_motion = img_motion[[1,0],:,:]
-            img_motion[1] = -img_motion[1] # flip y axis
+            target_image = self.transform(img_view_truth)
+            img_view = self.transform(img_view_input)
+            img_depth = self.transform(img_depth)[0:1, :, :]
+            img_motion = self.transform(img_motion)[0:2,:,:] 
+            img_motion[1] = -img_motion[1] # flip y axis 
             img_motion = img_motion * -1 # point backwards
 
             view_list.append(img_view)
@@ -159,10 +146,9 @@ class NSRRDataset(Dataset):
             motion_list.append(img_motion)
             truth_list.append(target_image)
             
-        # Ignore the last motion vector as it is not used
-        # Only return the most recent frame's truth
-        first_motion = torch.zeros_like(motion_list[0]) 
-        return view_list, depth_list, [first_motion] + motion_list[:-1], truth_list[0]
+        return view_list, depth_list, motion_list, truth_list
 
     def __len__(self) -> int:
         return len(self.data_list)
+
+
